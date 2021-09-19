644725b67e291df609463abcdadc2622f000f50f
Zer4 <abdurazzak.nurtazin@mail.ru>
2021-09-19 21:45:20 +0600

    message

--- Telegram normal bot.py
+++ Telegram normal bot.py
@@ -1,22 +1,24 @@
 import time
 import telebot
-from telebot import REPLY_MARKUP_TYPES, types
-#Фотография транспорта
+from telebot import types
+#Фотография транспортаpip install pyTelegramBotAPI
 # photo1 = open("photolink","rb")
 # photo2 = open("photolink","rb")
+#В words:0-Транспорт,1-Возмущение,2-Животные,3-Продукты,4-Фрукты,5-Овощи
 
 
 #0-Транспорт  1-Возмущение 2-Животные 3-Продукты 4-Фрукты 5-Овощи 
-words = ["🚦Транспорт - Ulaşım🚦\n\n🚁Вертолет - Helikopter\n\n✈️Самолет - Uçak\n\n🚉Поезд - Tren\n\n⛴Корабль - gemi \n\n🚤Лодка - Tekne\n\n🚲Велосипед - Bisiklet\n\n🚘Автомобиль - Araba\n\n🚌Автобус - Otobüs\n\n🚃Трамвай - Tramvay\n\n🏍Мотоцикл - Motor\n\n🛳Паром - Vapur\n\n🚕Такси - Taksi\n\n🚇Метро - Metro\n\n🚑Скорая - Ambulans",
-"👺Artık dayanamıyorum! - я не могу больше это терпеть!\n\n👺Sabrım taşıyor! - мое терпение лопнуло!\n\n👺Hele şükür! - ну наконец-то!\n\n👺Bu beni delirtiyor! - это меня раздражает!\n\n👺Rezalet! - это возмутительно!\n\n👺Çok ayıp! - как не стыдно!\n\n👺Of, bıktım artık! - как мне надоело!",
+words = ["🚦Транспорт - Ulaşım🚦\n\n🚁 Вертолет - Helikopter\n\n✈️ Самолет - Uçak\n\n🚉 Поезд - Tren\n\n⛴Корабль - gemi \n\n🚤 Лодка - Tekne\n\n🚲Велосипед - Bisiklet\n\n🚘 Автомобиль - Araba\n\n🚌 Автобус - Otobüs\n\n🚃 Трамвай - Tramvay\n\n🏍 Мотоцикл - Motor\n\n🛳 Паром - Vapur\n\n🚕 Такси - Taksi\n\n🚇 Метро - Metro\n\n🚑 Скорая - Ambulans",
+"👺 Artık dayanamıyorum! - я не могу больше это терпеть!\n\n👺 Sabrım taşıyor! - мое терпение лопнуло!\n\n👺 Hele şükür! - ну наконец-то!\n\n👺 Bu beni delirtiyor! - это меня раздражает!\n\n👺 Rezalet! - это возмутительно!\n\n👺 Çok ayıp! - как не стыдно!\n\n👺 Of, bıktım artık! - как мне надоело!",
 "🐶 Собака - Köpek\n\n🐈 Кошка - Kedi\n\n🐰 Кролик - Tavşan\n\n🐴 Лошадь - At\n\n🐦 Птица - Kuş\n\n🐯 Тигр - Kaplan\n\n🐻 Медведь - Ayı\n\n🐍 Змея - Yılan\n\n🐮 Корова - İnek\n\n🦁 Лев - Aslan",
 "🧀 Сыр - Peynir\n\n🥛 Молоко - Süt\n\n🥚  Яйца - Yumurta \n\n🍞 Хлеб - Ekmek\n\n🍦 Йогурт - Yoğurt\n\n🧈 Масло - Yağ\n\n🥩 Мясо - Et\n\n🍨 Мороженое - Dondurma\n\n🧋  Какао - Kako\n\n☕️ Кофе - Kahve",
-"🍎 Яблоко - Elma\n\n🍊 Апельсин - Portakal\n\n🥝Киви - Kivi\n\n🍉 Арбуз- Karpuz\n\n🍌 Банан - Muz\n\n🍇 Виноград - Üzüm\n\n🍈 Дыня - Kavun\n\n🍐 Груша - Armut\n\n🍓 Клубника - Çilek\n\n🍑Персик - Şeftali"]
+"🍎 Яблоко - Elma\n\n🍊 Апельсин - Portakal\n\n🥝 Киви - Kivi\n\n🍉 Арбуз- Karpuz\n\n🍌 Банан - Muz\n\n🍇 Виноград - Üzüm\n\n🍈 Дыня - Kavun\n\n🍐 Груша - Armut\n\n🍓 Клубника - Çilek\n\n🍑 Персик - Şeftali",
+"\n\n🥔 Картошка - Patates\n\n🥕 Морковка - havuç\n\n🥑 Авокадо - Avokado\n\n🥒 Огурец - Salatalık\n\n🍆 Баклажан - Patlıcan\n\n🧅 Лук - Soğan\n\n🍅 Помидор - Domates\n\n🧄 Чеснок - Sarımsak\n\n🥦 Брокколи - Brokoli"]
 
 bot = telebot.TeleBot('1722906153:AAHFAtbaBSD_9xwK9ORZyB-wyE5RhdzvRKY')
+
 print('Bot launched')
 @bot.message_handler(commands=['start'])
-# commands
 def send_welcome(message):
     username = message.from_user.first_name
     bot.send_message('1624372687', f'User {username} send message: {message.text}')
@@ -55,6 +57,8 @@ def get_text_messages(message):
         keyboard.add(key_4)
         key_5 = types.InlineKeyboardButton(text= 'Yemiş(Фрукты)',callback_data='fifth')
         keyboard.add(key_5)
+        key_6 = types.InlineKeyboardButton(text= 'Sebze(Овощи)',callback_data='sixth')
+        keyboard.add(key_6)
 
 
         bot.send_message(message.from_user.id, text='Выбери список', reply_markup=keyboard)
@@ -93,6 +97,9 @@ def callback_worker(call):
 
     elif call.data == "fifth":
         bot.send_message(call.message.chat.id,words[4])
+    
+    elif call.data == "sixth":
+        bot.send_message(call.message.chat.id,words[5])
 
 
 
